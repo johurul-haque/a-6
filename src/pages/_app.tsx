7509bc01080f1a4a2a2db92b231d002f9bcf54c2
Johurul Haque <johurulhaquejony@gmail.com>
2024-01-26 17:04:31 +0600

    initialized redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { store } from '@/redux/store';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import { Provider } from 'react-redux';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <main className={`${inter.className} container`}>
+        <Component {...pageProps} />
+      </main>
+    </Provider>
+  );
 }
