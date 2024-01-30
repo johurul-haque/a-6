4ea09c5df8df5c6f7118753b273e41a362c3b7a2
Johurul Haque <johurulhaquejony@gmail.com>
2024-01-30 18:17:07 +0600

    added origin and credentials in cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import { globalCatch } from '@middlewares/global-catch';
 import cors from 'cors';
 import express from 'express';
 import router from 'routes';
+import { env } from './config';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: env.CLIENT_DOMAIN, credentials: true }));
 
 app.use('/api', router);
 
