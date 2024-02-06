76b736ae1abcf84acb9c111e6b1532883702fd33
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-06 12:20:56 +0600

    removed unused file

--- src/lib/fetch-profile.ts
+++
@@ -1,9 +0,0 @@
-import { SERVER_DOMAIN } from '@/config';
-
-export function fetchProfile(cookie: string) {
-  return fetch(`${SERVER_DOMAIN}/profile`, {
-    headers: {
-      Authorization: cookie,
-    },
-  });
-}
