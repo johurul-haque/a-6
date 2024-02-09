ca064bf038450d5193081e497b3f65222397f56f
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-10 00:16:56 +0600

    bug: connected selling products route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { ProductRoutes } from './modules/product/product.route';
+import { SellRoutes } from './modules/sell/sell.route';
 import { UserRoutes } from './modules/user/user.route';
 
 const router = Router();
 
-router.use('/', UserRoutes);
+router.use('/', [UserRoutes, SellRoutes]);
 router.use('/products', ProductRoutes);
 
 export default router;
