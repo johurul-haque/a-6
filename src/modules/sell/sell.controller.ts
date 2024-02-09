06938fbcb31fd93bfbe9a2ec39c3f1227ebbd8a6
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-10 01:22:56 +0600

    transforming incoming payload using zod to custom date format(YYYY-MM-DD)

--- src/modules/sell/sell.controller.ts
+++ src/modules/sell/sell.controller.ts
@@ -1,11 +1,13 @@
 import { catchAsync } from '@/utils';
 import { sendResponse } from '@/utils/send-response';
 import * as sellServices from './sell.service';
+import { productSellSchema } from './sell.validation';
 
 export const sellProduct = catchAsync(async (req, res) => {
   const { productId } = req.params;
+  const payload = productSellSchema.parse(req.body);
 
-  const data = await sellServices.sell(productId, req.body);
+  const data = await sellServices.sell(productId, payload);
 
   sendResponse(res, {
     status: 201,
--- src/modules/sell/sell.validation.ts
+++ src/modules/sell/sell.validation.ts
@@ -2,6 +2,9 @@ import { z } from 'zod';
 
 export const productSellSchema = z.object({
   buyer_name: z.string(),
-  sold_on: z.string(),
+  sold_on: z.string().transform((value) => {
+    const date = new Date(value);
+    return date.toISOString().split('T')[0];
+  }),
   quantity_sold: z.number(),
 });
