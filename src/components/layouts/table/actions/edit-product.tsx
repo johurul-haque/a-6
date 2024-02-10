ac6830e0f85c1175a0382262451a544d6467eae2
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-10 11:14:15 +0600

    refactored edit modal to activate inside dropdown

--- src/components/layouts/table/actions/edit-product.tsx
+++ src/components/layouts/table/actions/edit-product.tsx
@@ -1,28 +1,21 @@
 import { Button } from '@/components/ui/button';
 import * as D from '@/components/ui/dialog';
-import { DropdownMenuShortcut } from '@/components/ui/dropdown-menu';
 import { Form } from '@/components/ui/form';
 import { useUpdateProductMutation } from '@/redux/api/products';
 import { productSchema } from '@/schema/products-form-schema';
 import { Product, ProductSchema } from '@/types/product';
+import { SetStateActionType } from '@/types/set-state-action';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Edit } from 'lucide-react';
-import { SetStateAction, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { ProductFormFields } from '../../product-form-fields';
 
 type EditProductProps = {
-  setIsFormVisible: React.Dispatch<SetStateAction<boolean>>;
-  openDropdown: React.Dispatch<SetStateAction<boolean>>;
   row: Product;
+  setIsOpen: SetStateActionType<boolean>;
 };
 
-export function EditProduct({
-  row,
-  setIsFormVisible,
-  openDropdown: closeDropdown,
-}: EditProductProps) {
-  const [isOpen, setIsOpen] = useState(false);
+export function EditProduct({ row, setIsOpen }: EditProductProps) {
   const [updateProduct, { isLoading }] = useUpdateProductMutation();
 
   const form = useForm<ProductSchema>({
@@ -32,54 +25,33 @@ export function EditProduct({
 
   return (
     <>
-      <D.Dialog
-        open={isOpen}
-        onOpenChange={(open) => {
-          setIsOpen(open);
-          setIsFormVisible(open);
-        }}
-      >
-        <D.DialogTrigger className="flex items-center justify-between w-full">
-          Edit
-          <DropdownMenuShortcut>
-            <Edit className="size-4 stroke-current" />
-          </DropdownMenuShortcut>
-        </D.DialogTrigger>
-        <D.DialogContent className="sm:max-w-[550px]">
-          <D.DialogHeader className="mt-2">
-            <D.DialogTitle className="flex gap-2">
-              <Edit className="size-5" />
-              Edit product
-            </D.DialogTitle>
+      <D.DialogContent className="sm:max-w-[550px]">
+        <D.DialogHeader className="mt-2">
+          <D.DialogTitle className="flex gap-2">
+            <Edit className="size-5" />
+            Edit product
+          </D.DialogTitle>
 
-            <D.DialogDescription>
-              Modify details of the product here. Click save when you are done.
-            </D.DialogDescription>
-          </D.DialogHeader>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit((values) => {
-                updateProduct({ body: values, id: row._id });
-                setIsOpen(false);
-                setIsFormVisible(false);
-                closeDropdown(false);
-                form.reset();
-              })}
-              className="grid gap-3"
-            >
-              <ProductFormFields form={form} isLoading={isLoading} />
+          <D.DialogDescription>
+            Modify details of the product here. Click save when you are done.
+          </D.DialogDescription>
+        </D.DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit((values) => {
+              updateProduct({ body: values, id: row._id });
+              setIsOpen(false);
+            })}
+            className="grid gap-3"
+          >
+            <ProductFormFields form={form} isLoading={isLoading} />
 
-              <Button
-                disabled={isLoading}
-                type="submit"
-                className="w-full mt-3"
-              >
-                Save
-              </Button>
-            </form>
-          </Form>
-        </D.DialogContent>
-      </D.Dialog>
+            <Button disabled={isLoading} type="submit" className="w-full mt-3">
+              Save
+            </Button>
+          </form>
+        </Form>
+      </D.DialogContent>
     </>
   );
 }
--- src/components/layouts/table/data-table-row-actions.tsx
+++ src/components/layouts/table/data-table-row-actions.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import * as D from '@/components/ui/dialog';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,14 +7,13 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { useDeleteProductMutation } from '@/redux/api/products';
 import { Product } from '@/types/product';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { Row } from '@tanstack/react-table';
-import { useClickAway } from '@uidotdev/usehooks';
-import { Trash2 } from 'lucide-react';
+import { Edit, Trash2 } from 'lucide-react';
 import { useState } from 'react';
 import { EditProduct } from './actions/edit-product';
-import { useDeleteProductMutation } from '@/redux/api/products';
 
 interface DataTableRowActionsProps {
   row: Row<Product>;
@@ -23,48 +23,50 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [isFormVisible, setIsFormVisible] = useState(false);
 
-  const ref = useClickAway<HTMLDivElement>(() => {
-    if (!isFormVisible) {
-      setIsOpen(false);
-    }
-  });
+  // const ref = useClickAway<HTMLDivElement>(() => {
+  //   if (!isFormVisible) {
+  //     setIsOpen(false);
+  //   }
+  // });
 
   const [deleteProduct, { isLoading }] = useDeleteProductMutation();
 
   return (
-    <DropdownMenu open={isOpen} modal={false}>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-          onClick={() => setIsOpen(true)}
-        >
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent ref={ref} align="start" className="w-[160px]">
-        <DropdownMenuItem>
-          <EditProduct
-            row={row.original}
-            setIsFormVisible={setIsFormVisible}
-            openDropdown={setIsOpen}
-          />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="w-full text-rose-600 hover:bg-rose-100 hover:text-rose-600 focus:bg-rose-100 focus:text-rose-600"
-          onClick={() => {
-            deleteProduct(row.original._id);
-            setIsOpen(false);
-          }}
-          disabled={isLoading}
-        >
-          Delete
-          <DropdownMenuShortcut>
-            <Trash2 className="size-4 stroke-current" />
-          </DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <D.Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          >
+            <DotsHorizontalIcon className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start" className="w-[160px]">
+          <D.DialogTrigger asChild>
+            <DropdownMenuItem>
+              Edit
+              <DropdownMenuShortcut>
+                <Edit className="size-4 stroke-current" />
+              </DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </D.DialogTrigger>
+          <DropdownMenuItem
+            className="w-full text-rose-600 hover:bg-rose-100 hover:text-rose-600 focus:bg-rose-100 focus:text-rose-600"
+            onClick={() => {
+              deleteProduct(row.original._id);
+            }}
+            disabled={isLoading}
+          >
+            Delete
+            <DropdownMenuShortcut>
+              <Trash2 className="size-4 stroke-current" />
+            </DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <EditProduct setIsOpen={setIsOpen} row={row.original} />
+    </D.Dialog>
   );
 }
