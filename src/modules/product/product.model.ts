f123addc8849527fe3a5b073493a2dc6d9b3dc7e
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-14 10:34:11 +0600

    not deleting products after quantity reaching zero

--- src/modules/product/product.model.ts
+++ src/modules/product/product.model.ts
@@ -30,16 +30,4 @@ const productModelSchema = new Schema<TProductModel>(
   }
 );
 
-productModelSchema.pre('updateOne', async function (next) {
-  const query = this.getQuery();
-
-  const product = await ProductModel.findOne(query);
-
-  if (product && product.quantity <= 0) {
-    await ProductModel.deleteOne(query);
-  }
-
-  next();
-});
-
 export const ProductModel = model('product', productModelSchema);
