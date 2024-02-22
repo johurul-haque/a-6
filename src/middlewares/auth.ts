6c98cfbc155dd01e6463abc60934dce1df2baf1b
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-22 19:23:50 +0600

    Refactored auth middleware to use role for authorization

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,24 +3,35 @@ import { jwtPayload } from '@/modules/user/user.validation';
 import { AppError, catchAsync } from '@/utils';
 import jwt from 'jsonwebtoken';
 
-export const verifyToken = catchAsync((req, res, next) => {
-  try {
+export function verifyToken (role?: 'user' | 'admin') {
+  return catchAsync((req, res, next) => {
+    try {
+      const bearerHeader: string =
+        req.cookies.token || req.headers.authorization;
 
-    const bearerHeader: string = req.cookies.token || req.headers.authorization;
+      if (!bearerHeader || !bearerHeader.startsWith('Bearer'))
+        throw new Error();
 
-    if (!bearerHeader || !bearerHeader.startsWith('Bearer')) throw new Error();
+      // Extract the token by removing the "Bearer " prefix
+      const token = bearerHeader.split(' ')[1];
 
-    // Extract the token by removing the "Bearer " prefix
-    const token = bearerHeader.split(' ')[1];
+      jwt.verify(token, env.JWT_SECRET, (err, decoded) => {
+        if (err) throw err;
 
-    jwt.verify(token, env.JWT_SECRET, (err, decoded) => {
-      if (err) throw err;
+        req.jwtPayload = jwtPayload.parse(decoded);
 
-      req.jwtPayload = jwtPayload.parse(decoded);
-
-      next();
-    });
-  } catch (err) {
-    throw new AppError(401, 'Unauthorized access.');
-  }
-});
+        if (role) {
+          if (role === req.jwtPayload.role) {
+            next();
+          } else {
+            throw new Error();
+          }
+        } else {
+          next();
+        }
+      });
+    } catch (err) {
+      throw new AppError(401, 'Unauthorized access.');
+    }
+  });
+}
