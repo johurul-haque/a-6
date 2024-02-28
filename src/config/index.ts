6685162b70a19d395f64862e0de0b30bb40e0207
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-28 14:58:38 +0600

    changed api server url on production

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const SERVER_DOMAIN =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : 'https://api-a-5.vercel.app/api';
+    : 'https://api-a-6.vercel.app/api';
