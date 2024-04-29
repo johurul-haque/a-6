fecf0979de5f8620a33af2c823f55dff95096da8
Johurul Haque <johurulhaquejony@gmail.com>
2024-04-30 01:33:05 +0600

    importing fonts from seperate file

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,9 @@
+import { inter } from '@/components/fonts';
 import { NextHead } from '@/components/next-head';
 import { store } from '@/redux/store';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
 import { Provider } from 'react-redux';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
